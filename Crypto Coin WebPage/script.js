14a903089166451715ea10f8c8472afc1dd8ac5d
Khuluza Tshabalala <khuluza0@gmail.com>
2025-05-23 10:03:23 +0200

    Created script.js

---
+++ Crypto Coin WebPage/script.js
@@ -0,0 +1,5 @@
+// Simple JavaScript for dark mode toggle (optional)
+document.querySelector('button').addEventListener('click', function() {
+    // You can add dark mode functionality here
+    console.log('Dark mode toggle clicked');
+});
